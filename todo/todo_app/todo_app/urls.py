bd5014e6a5a6b296d49fb8ec2004202830d552df
Stephen842 <ugotachisomstephen@gmail.com>
2024-01-16 12:04:14 +0100

    hotfix

--- todo/todo_app/todo_app/urls.py
+++ todo/todo_app/todo_app/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.todo, name = 'todo'),
-
-    path('del/<str:item_id>', views.remove, name = 'del'),
+    path('', include('todo.urls')),
 ]
