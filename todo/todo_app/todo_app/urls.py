3d8b33619acba6cdc068d248ddfa8da2a1051356
Stephen842 <ugotachisomstephen@gmail.com>
2024-01-16 11:52:56 +0100

    hot fix

--- todo/todo_app/todo_app/urls.py
+++ todo/todo_app/todo_app/urls.py
@@ -19,4 +19,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.todo, name = 'todo'),
+
+    path('del/<str:item_id>', views.remove, name = 'del'),
 ]
