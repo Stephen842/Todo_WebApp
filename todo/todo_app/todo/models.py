57617fc04560cbc07fa291bcb0ad482210824c61
Stephen842 <ugotachisomstephen@gmail.com>
2024-01-16 11:56:18 +0100

    hotfix

--- todo/todo_app/todo/models.py
+++ todo/todo_app/todo/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Todo(models.Model):
     title = models.CharField(max_length = 200)
     details = models.TextField()
-    date = models.DateTimeField(default = timmezone.now)
+    date = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
         return self.title
