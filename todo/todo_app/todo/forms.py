e10155207ed370dc7ea5054d6b540c66fb41e6cb
Stephen842 <ugotachisomstephen@gmail.com>
2024-09-06 16:27:31 +0100

    I finished working on its CSS file, and every other thing is working as expected, both frontend and backend, with the few update been pushed, I can now move ahead with other project cos though in it imperfection it is still something one can start with.

--- todo/todo_app/todo/forms.py
+++ todo/todo_app/todo/forms.py
@@ -11,12 +11,6 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'details', 'completed'] 
-        widget = {
-                'title': forms.TextInput(attrs = {'class': 'title2', 'placeholder': 'Enter Title'}),
-                'details': forms.Textarea(attrs = {'class': 'details', 'placeholder': 'Enter Description'}),
-                'completed': forms.CheckboxInput(attrs = {'class': 'checkbox'}),
-                'date': forms.DateInput(attrs = {'class': 'date'})
-                }
 
 class SearchForm(forms.Form):
     search_query = forms.CharField(max_length = 250)
